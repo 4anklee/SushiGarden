0b1de876256833a0240624708ca24771635cac08
Frank Lee <4anklee@gmail.com>
2023-11-01 10:40:14 -0600

    photo-gallery

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ document.getElementById('prev').onclick = function (){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
+
+const menu_btn = document.querySelector('.hamburger');
+const nav_bar = document.querySelector('.mobile-nav')
+
+menu_btn.addEventListener('click', function (){
+    menu_btn.classList.toggle('is-active');
+    nav_bar.classList.toggle('is-active')
+});
