c45eda266efebd085432d5434d0eeaedcfb24a4d
Frank Lee <4anklee@gmail.com>
2023-11-01 22:19:32 -0600

    photo-gallery

--- script.js
+++ script.js
@@ -8,13 +8,29 @@ document.getElementById('prev').onclick = function (){
 }
 
 const menu_btn = document.querySelector('.hamburger');
-const nav_bar = document.querySelector('.mobile-nav')
+const nav_bar = document.querySelector('.mobile-nav');
 
 menu_btn.addEventListener('click', function (){
     menu_btn.classList.toggle('is-active');
     nav_bar.classList.toggle('is-active')
+    const mobileNavLinks = document.querySelectorAll('.mobile-nav a');
+
+    setTimeout(function () {
+        mobileNavLinks.forEach(function (link, index) {
+            setTimeout(function () {
+                link.style.transform = 'translateX(0)';
+                link.style.opacity = '1';
+            }, index * 100);
+        });
+    }, 100);
 });
 
+document.addEventListener('click', function (e) {
+    if (!menu_btn.contains(e.target) && !nav_bar.contains(e.target)) {
+        menu_btn.classList.remove('is-active');
+        nav_bar.classList.remove('is-active');
+    }
+});
 
 let isPlaying =true;
 let intervalId;
@@ -29,7 +45,13 @@ function autoChangeImage() {
 
 autoChangeImage();
 
-document.getElementById('pause').onclick = function (){
-    isPlaying = !isPlaying
-    const pauseButton = document.getElementById('pause')
+document.getElementById('pause').onclick = function() {
+    const pauseButton = document.getElementById('pause');
+    const pauseIcon = pauseButton.querySelector('.fa-pause');
+    const playIcon = pauseButton.querySelector('.fa-play');
+
+    playIcon.style.display = isPlaying ? 'block' : 'none';
+    pauseIcon.style.display = isPlaying ? 'none' : 'block';
+
+    isPlaying = !isPlaying;
 }
